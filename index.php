14b96e0430489377f0f797775292b725d519a3ad
Bowen Lu <bluinc@ubuntu.(none)>
2013-08-04 02:25:47 -0700

    yo

--- index.php
+++ index.php
@@ -54,30 +54,30 @@ if ($user_id) {
     }
   }
 
-if(file_exists('tmp/last_post.data')) {
-  $last_post = unserialize(file_get_contents('tmp/last_post.data'));
+if(file_exists('./tmp/last_post.data')) {
+  $last_post = unserialize(file_get_contents('./tmp/last_post.data'));
 } else {
-  file_put_contents('tmp/last_post.data', serialize(value));
+  file_put_contents('./tmp/last_post.data', serialize(value));
 }
 
 
-if(file_exists('tmp/fb_home.data')) {
-  $home = unserialize(file_get_contents('tmp/fb_home.data'));
-  if(file_exists('tmp/last_post.data')) {
-    $last_post = unserialize(file_get_contents('tmp/last_post.data'));
+if(file_exists('./tmp/fb_home.data')) {
+  $home = unserialize(file_get_contents('./tmp/fb_home.data'));
+  if(file_exists('./tmp/last_post.data')) {
+    $last_post = unserialize(file_get_contents('./tmp/last_post.data'));
     if((idx($home[0]),'id') != $last_post) {
       $home = idx($facebook->api('/me/home?limit=100'), 'data', array());
-      file_put_contents('tmp/last_post.data', serialize(idx($home[0]),'id'));
+      file_put_contents('./tmp/last_post.data', serialize(idx($home[0]),'id'));
     }
   } else {
     $home = idx($facebook->api('/me/home?limit=100'), 'data', array());
-    file_put_contents('tmp/last_post.data', serialize(idx($home[0]),'id'));
+    file_put_contents('./tmp/last_post.data', serialize(idx($home[0]),'id'));
   }
 }
 
 if (!$home) { // cache doesn't exist or is older than 10 mins
   $home = idx($facebook->api('/me/home?limit=100'), 'data', array());
-  file_put_contents('tmp/fb_home.data', serialize($home));
+  file_put_contents('./tmp/fb_home.data', serialize($home));
 }
   
 
