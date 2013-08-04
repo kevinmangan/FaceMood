f2a0884a27266afda7e61f35dc79f9a632c2896b
Kevin Mangan <kmm2256@columbia.edu>
2013-08-04 07:56:17 -0400

    trying out refresh button

--- index.php
+++ index.php
@@ -149,12 +149,13 @@ function get_data($url) {
     <![endif]-->
 <script>
     $( document ).ready(function() {
-    
+      
+
         $("#positiveRefresh").click(function() {
             FB.api('/me/home?limit=10', function(response) {
               jQuery.each(response.post, function(){
-                  if(document.getElementById("post" + this.id) == null){
-                      $("#positive .friends").prepend("<div class=\"panel\"><a href=\"https:www.facebook.com/" + this.from.id + "target="_top"><img src=\"https://graph.facebook.com/" + this.from.id + "/picture?type=square\" alt=\""  + this.from.name . "\"> +  this.from.name + "</a><br><br>" +  this.message + "<hr> <div class=\"row collapse\"> <input id=\"" + this.id + "\" type=\"text\" placeholder=\"Comment on their mood...\">  <a href=\"#\"  class=\"button prefix\">Post</a></div> </div>");
+                  if(document.getElementById("post" + this.id) != null){
+                      $("#positive .friends").prepend("<div class=\"panel\"><a href=\"https:www.facebook.com/" + this.from.id + "target="_top"><img src=\"https://graph.facebook.com/" + this.from.id + "/picture?type=square\" alt=\""  + this.from.name . "\">" +  this.from.name + "</a><br><br>" +  this.message + "<hr> <div class=\"row collapse\"> <input id=\"" + this.id + "\" type=\"text\" placeholder=\"Comment on their mood...\">  <a href=\"#\"  class=\"button prefix\">Post</a></div> </div>");
                   }
               });
             });
@@ -306,7 +307,7 @@ function get_data($url) {
   <div class="container" id="container1">  
     <div class="small-2 large-4 columns" id="negative" >
           <br>
-          <a href="#" id="negativeRefresh" class="button">Refresh</a>
+          <a id="negativeRefresh" class="button">Refresh</a>
           <br>
           <div class="friends">
            
@@ -316,7 +317,7 @@ function get_data($url) {
     
     <div class="small-4 large-4 columns" id="neutral" >
           <br>
-          <a href="#" id="neutralRefresh" class="button">Refresh</a>
+          <a id="neutralRefresh" class="button">Refresh</a>
           <br>
           <div class="friends">
               
@@ -325,7 +326,7 @@ function get_data($url) {
     
     <div class="small-6 large-4 columns" id="positive" >
             <br>
-            <a href="#" id="positiveRefresh" class="button">Refresh</a>
+            <a id="positiveRefresh" class="button">Refresh</a>
             <br>
             <div class="friends">
       
